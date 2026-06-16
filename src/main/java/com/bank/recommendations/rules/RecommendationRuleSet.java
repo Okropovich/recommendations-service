db163879bcf76d4f8fc4b2bf57a18dd852f0b600
Okropovich <89385037063k@gmail.com>
2026-06-17 00:41:28 +0300

    Initialize empty main branch

--- src/main/java/com/bank/recommendations/rules/RecommendationRuleSet.java
+++
@@ -1,9 +0,0 @@
-package com.bank.recommendations.rules;
-
-import com.bank.recommendations.dto.ProductDto;
-import java.util.Optional;
-import java.util.UUID;
-
-public interface RecommendationRuleSet {
-    Optional<ProductDto> checkRequirement(UUID userId);
-}
